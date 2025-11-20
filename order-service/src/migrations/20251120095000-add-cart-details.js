53377391d431e55de41f22aa02c9e2fd2735d2d8
DangQui <dangdinhqui2001@gmail.com>
2025-11-20 21:56:10 +0700

    main

--- order-service/src/migrations/20251120095000-add-cart-details.js
+++ order-service/src/migrations/20251120095000-add-cart-details.js
@@ -1,10 +1,39 @@
-module.exports = function internalAuth(req, res, next) {
-    const provided = req.headers['x-internal-secret'];
-    const expected = process.env.INTERNAL_SERVICE_SECRET || 'wowwraps_internal_secret';
-  
-    if (!provided || provided !== expected) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-  
-    return next();
-  };
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn('carts', 'orderType', {
+      type: Sequelize.STRING,
+      allowNull: false,
+      defaultValue: 'delivery'
+    });
+
+    await queryInterface.addColumn('carts', 'customerName', {
+      type: Sequelize.STRING,
+      allowNull: true
+    });
+
+    await queryInterface.addColumn('carts', 'customerPhone', {
+      type: Sequelize.STRING,
+      allowNull: true
+    });
+
+    await queryInterface.addColumn('carts', 'deliveryAddress', {
+      type: Sequelize.TEXT,
+      allowNull: true
+    });
+
+    await queryInterface.addColumn('carts', 'deliveryNote', {
+      type: Sequelize.TEXT,
+      allowNull: true
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.removeColumn('carts', 'deliveryNote');
+    await queryInterface.removeColumn('carts', 'deliveryAddress');
+    await queryInterface.removeColumn('carts', 'customerPhone');
+    await queryInterface.removeColumn('carts', 'customerName');
+    await queryInterface.removeColumn('carts', 'orderType');
+  }
+};
